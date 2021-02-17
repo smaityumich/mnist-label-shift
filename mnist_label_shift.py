e52bbdeeca888c731a1f148bc488d499611b4ba9
Subha Maity <smaity@smaity-mbp.local>
2021-02-17 11:01:28 -0500

    Done

--- mnist_label_shift.py
+++ mnist_label_shift.py
@@ -10,6 +10,7 @@ def label_shift(n_target = 1000, n_source = 20000, pi_target = 0.1,\
 
     x_source, y_source = mixture(n = n_source, pi = pi_source, flatten=False)
     x_target, y_target = mixture(n = n_target, pi = pi_target, flatten=False)
+    x_val, y_val = mixture(n = n_target, pi = pi_target, flatten=False)
 
     loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
@@ -89,7 +90,7 @@ def label_shift(n_target = 1000, n_source = 20000, pi_target = 0.1,\
             ])
     model_gs.compile(optimizer='adam', loss=loss_fn, metrics=['accuracy'])
     model_gs.fit(x_target, y_target, epochs=epochs)
-    accuracy_gs = model_gs.evaluate(x_target, y_target, verbose=2)[1]
+    accuracy_gs = model_gs.evaluate(x_val, y_val, verbose=2)[1]
 
     return accuracy_pilot, accuracy_reweighted, accuracy_label_shift, accuracy_train_pilot,\
         accuracy_train_reweighted, accuracy_train_ls, accuracy_gs
